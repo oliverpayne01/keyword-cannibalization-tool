3832013ece25416b6da5fe3c86e7ff6cf57285bc
Oliver Payne <oliver.payne@MCD-APL-0156.local>
2025-12-06 00:55:39 -0700

    Test data preprocessing

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import re
 
-csv_path = "/Users/oliver.payne/Desktop/Pages.csv"
+csv_path = "/Users/oliver.payne/Desktop/Queries.csv"
 df = pd.read_csv(csv_path)
-print (df.info())
 
-# Filter data
+def preprocess_data():
+  def non_branded_rows():
+    return df[~df["Top queries"].str.contains("colonial")]
+
+  def rows_with_clicks():
+    return non_branded_rows()[df["Clicks"] > 0]
+
+  return rows_with_clicks()
+
+df = preprocess_data()
+print (df)
 
 # Identify cannibalization
-
+# Create a list of cannibalization candidates
+# Calculate clicks per query / click share etc.
+# Print results
