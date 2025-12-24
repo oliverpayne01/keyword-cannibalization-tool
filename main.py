5bf93a90488c82f1b033905d58ec6e845af4faa2
Oliver Payne <oliver@oliverpayne.me>
2025-12-24 12:24:58 -0700

    Update data frame naming convention for clarity in main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
 csv_path = "/Users/oliver.payne/Desktop/keywords.csv"
-df = pd.read_csv(csv_path)
+df_raw = pd.read_csv(csv_path)
 
-grouped = (
-    df.groupby("Keyword")["URL"].agg(lambda x: ", ".join(sorted(set(x)))).reset_index()
+df_grouped = (
+    df_raw.groupby("Keyword")["URL"].agg(lambda x: ", ".join(sorted(set(x)))).reset_index()
 )
 
-grouped["url_count"] = grouped["URL"].apply(lambda x: len(x.split(", ")))
+df_grouped["url_count"] = df_grouped["URL"].apply(lambda x: len(x.split(", ")))
+df_rows_to_remove = df_grouped[df_grouped["url_count"] == 1].index
+df_filtered = df_grouped.drop(df_rows_to_remove)
 
-grouped.to_csv("output.csv")
+df_filtered.to_csv("output.csv")
