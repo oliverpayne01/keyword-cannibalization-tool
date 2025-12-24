957a2359956b8a32772464743c6bc3d241784399
Oliver Payne <oliver@oliverpayne.me>
2025-12-24 12:26:45 -0700

    Assign a new variable for df_with_url_count to improve clarity in main.py

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ df_grouped = (
     df_raw.groupby("Keyword")["URL"].agg(lambda x: ", ".join(sorted(set(x)))).reset_index()
 )
 
-df_grouped["url_count"] = df_grouped["URL"].apply(lambda x: len(x.split(", ")))
-df_rows_to_remove = df_grouped[df_grouped["url_count"] == 1].index
-df_filtered = df_grouped.drop(df_rows_to_remove)
+df_with_url_count = df_grouped
+df_with_url_count["url_count"] = df_grouped["URL"].apply(lambda x: len(x.split(", ")))
+df_rows_to_remove = df_with_url_count[df_with_url_count["url_count"] == 1].index
+df_filtered = df_with_url_count.drop(df_rows_to_remove)
 
 df_filtered.to_csv("output.csv")
